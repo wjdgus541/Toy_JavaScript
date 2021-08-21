5be5a02c408e748069347ec720b93a38e8ded3b7
wjdgus541 <70759667+wjdgus541@users.noreply.github.com>
2021-08-21 22:43:55 +0900

    FEAT:Add quotes and background

--- practice/js/clock.js
+++ practice/js/clock.js
@@ -5,7 +5,7 @@ function setTimeout() {
     const hours = String(date.getHours()).padStart(2,'0');
     const minutes = String(date.getMinutes()).padStart(2,'0');
     const seconds = String(date.getSeconds()).padStart(2,'0');
-    clock.innerText = hours>12? `0${hours-12}:${minutes}:${seconds} pm`:`${hours}:${minutes}:${seconds} am`
+    clock.innerText = hours>12? `${hours-12}:${minutes}:${seconds} pm`:`${hours}:${minutes}:${seconds} am`
 }
 
 setTimeout();
