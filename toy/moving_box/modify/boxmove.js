299725a9c56b4732df8594eec8c8f0db74906cef
wjdgus541 <nnn5425@naver.com>
2021-09-16 18:28:56 +0900

    fix offset reset

--- toy/moving_box/modify/boxmove.js
+++ toy/moving_box/modify/boxmove.js
@@ -5,10 +5,10 @@ const miniBox = document.querySelector('.miniBox');
 const left_topX = leftBox.getBoundingClientRect().x + 9 + 150;
 const right_topX = rightBox.getBoundingClientRect().x + 9 + 150;
 
-console.log(left_topX);
 
-
+//이동할 거리
 const offset = {x:0, y:0};
+//드래그 시작 지점의 마우스 포인터 위치
 const initailMousePos = {x:0, y:0};
 
 const move = e => {
@@ -19,26 +19,28 @@ const move = e => {
 }
 
 const mouseDown = e => {
-    initailMousePos.x = e.clientX - offset.x;
-    initailMousePos.y = e.clientY - offset.y;
-
+        initailMousePos.x = e.clientX - offset.x;
+        initailMousePos.y = e.clientY - offset.y;
     document.addEventListener('mousemove', move);
 }
 
 miniBox.addEventListener('mousedown', mouseDown);
 
-document.addEventListener('mouseup', e => {
+const mouseUp = e => {
     document.removeEventListener('mousemove', move);
+    document.removeEventListener('mousedown', mouseDown);
+
     const miniBox_center = miniBox.getBoundingClientRect().x + 150;
     const leftSpace = Math.abs(e.clientX-left_topX);
     const rightSpace = Math.abs(e.clientX-right_topX);
-    console.log(leftSpace, rightSpace, e.clientX);
     if (leftSpace < rightSpace) {
         miniBox.style.transform = `translateY(${offset.y}px)`;
         leftBox.appendChild(miniBox);
-
     }else {
         miniBox.style.transform = `translateY(${offset.y}px)`;
         rightBox.appendChild(miniBox);
     }
-});
+    offset.x = 0;
+}
+
+document.addEventListener('mouseup', mouseUp);
